fef9586c99b2f65a8f5a3a01ae20165b2c8e8d53
Jamestown-Mac <jamescmcintyre@gmail.com>
2009-10-17 06:41:16 -0700

    Added descriptions to stories. Added auto-voting for new submissions.

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,6 +4,8 @@ class Story < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :link
   
+  after_create :create_initial_vote
+  
   has_many :votes do
     def latest
       find :all, :order => 'id DESC', :limit => 3
@@ -13,4 +15,9 @@ class Story < ActiveRecord::Base
   def to_param
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
   end
+  
+  protected
+    def create_initial_vote
+      votes.create :user => user
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091017092050) do
+ActiveRecord::Schema.define(:version => 20091017132043) do
 
   create_table "stories", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20091017092050) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "votes_count", :default => 0
+    t.text     "description"
   end
 
   create_table "users", :force => true do |t|
