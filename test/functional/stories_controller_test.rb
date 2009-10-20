e06cea33597bd77b755ab87b0e44659b9e154bc0
Jamestown-Mac <jamescmcintyre@gmail.com>
2009-10-20 00:09:10 -0700

    Added tests for the new user pages.
    Added tests for a couple of things. The first is the counter cache that
    was added to the story model (for the vote list). Secondly, added a test
    to ensure the auto-voting functionality for newly submitted stories. Also,
    added test for the newly remodeled index page to ensure that the correct
    stories are displayed (only those that are above the threshold for votes
    should appear on the homepage). In addition, tests have been added for
    the voting bin page. Lastly, tests have been added to test the display
    and functionality of the new user pages.

--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -6,7 +6,14 @@ class StoriesControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_template 'index'
-    assert_not_nil assigns(:story)
+    assert_not_nil assigns(:stories)
+  end
+  
+  def test_should_show_bin
+    get :bin
+    assert_response :success
+    assert_template 'index'
+    assert_not_nil assigns(:stories)
   end
   
   def test_should_show_new
@@ -18,7 +25,7 @@ class StoriesControllerTest < ActionController::TestCase
   
   def test_should_show_new_form
     get_with_user :new
-    assert_select 'form p', :count => 3
+    assert_select 'form p', :count => 4
   end
   
   def test_should_add_story
@@ -52,20 +59,25 @@ class StoriesControllerTest < ActionController::TestCase
   end
   
   def test_should_show_story_vote_elements
-    get :show, :id => stories(:one)
+    get_with_user :show, :id => stories(:one)
     assert_select 'h2 span#vote_score'
     assert_select 'ul#vote_history li', :count => 2
     assert_select 'div#vote_form form'
   end
   
+  def test_should_not_show_vote_button_if_not_logged_in
+    get :show, :id => stories(:one)
+    assert_select 'div#vote_link', false
+  end
+  
   def test_should_show_story_submitter
     get :show, :id => stories(:one)
-    assert_select 'p.submitted_by span', 'patrick'
+    assert_select 'p.submitted_by a', 'patrick'
   end
   
   def test_should_show_navigation_menu
     get :index
-    assert_select 'ul#navigation li', :count => 2
+    assert_select 'ul#navigation li', :count => 3
   end
   
   def test_should_indicate_not_logged_in
@@ -91,6 +103,32 @@ class StoriesControllerTest < ActionController::TestCase
       :link => 'http://www.story-with-user.com/'
     }
     assert_equal users(:patrick), assigns(:story).user
+  end
+  
+  def test_should_only_list_promoted_on_index
+    get :index
+    assert_equal [stories(:promoted)], assigns(:stories)
+  end
+  
+  def test_should_only_list_unpromoted_in_bin
+    get :bin
+    assert_equal [stories(:one), stories(:two)], assigns(:stories)
+  end
+  
+  def test_should_use_story_index_as_default
+    assert_recognizes({:controller => 'stories', :action => 'index'}, '/')
+  end
+  
+  def test_should_show_story_on_index
+    get :index
+    assert_select 'h2', 'Showing 1 front-page story'
+    assert_select 'div#content div.story', :count => 1
+  end
+  
+  def test_should_show_stories_in_bin
+    get :bin
+    assert_select 'h2', 'Showing 2 upcoming stories'
+    assert_select 'div#content div.story', :count => 2
   end
   
 end
